8d1fea7fe8d127c0f7d140701cffe2a1d7189595
Iain R. Learmonth <irl@fsfe.org>
2021-02-10 14:57:50 +0000

    Allow abstract Store class to be used for dev

--- cleaninsights/store.py
+++ cleaninsights/store.py
@@ -17,12 +17,19 @@ class Store:
             raise RuntimeError("Storage doesn't exist or isn't readable.")
         if 'events' in data:
             self.events = [Event(**e) for e in data['events']]
+        else:
+            self.events = []
         if 'visits' in data:
             self.visits = [Visit(**v) for v in data['visits']]
+        else:
+            self.visits = []
 
     def load(self, *args,
              **kwargs) -> Optional[List[Dict[str, Union[str, int, float]]]]:
-        raise NotImplementedError
+        if len(args) > 0 and args[0] == "memory":
+            return dict()
+        else:
+            raise NotImplementedError
 
     def persist(self) -> None:
         raise NotImplementedError
